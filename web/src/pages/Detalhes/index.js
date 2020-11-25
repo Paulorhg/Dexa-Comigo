2e3629f5fba45729ef5cc5cfa19a71e258c19825
pedrovitro <pedrovitro@hotmail.com>
2020-11-25 11:41:11 -0300

    25-11,2

--- web/src/pages/Detalhes/index.js
+++ web/src/pages/Detalhes/index.js
@@ -6,6 +6,11 @@ import Template from '../../components/Template'
 import api from '../../services/api'
 
 import './detalhes.css'
+import carne from '../../assets/carne.png'
+import cerveja from '../../assets/cerveja.png'
+import linguica from '../../assets/linguica.png'
+import frango from '../../assets/frango.png'
+import refrigerante from '../../assets/refrigerante.png'
 
 export default function Detalhes() {
 
@@ -34,7 +39,7 @@ export default function Detalhes() {
     }, [loggedUser, params.id]);
 
     function FormataData2(data){
-        var dataFormatada = data.substr(8,2) + "-" + data.substr(5,2) + "-" + data.substr(0,4)
+        var dataFormatada = data.substr(8,2) + "/" + data.substr(5,2) + "/" + data.substr(0,4)
         return dataFormatada;
     }
 
@@ -53,7 +58,71 @@ export default function Detalhes() {
                    <h2>Carregando...</h2>
                </div>
             }
-            </div>
+              
+                <div id="form-criar">
+                    <div id="caracteristica">
+                            <div id="nome">
+                                    <label>Nome</label>
+                            </div>  
+
+                            <div id="data">    
+                                    <label>Data</label>
+                            </div>
+                            <div id="participantes">       
+                                    <label>Participantes</label>
+                            </div>  
+                    </div>
+                
+
+                      
+
+
+
+                        <div id="arrumar-itens1">
+                        <label>Itens</label>
+
+                        <div id="destino-itens">
+                        <div >
+                         <img id="carne" src={carne} alt="carne"/>
+                            <div id="escrita-carne">
+                                <h3> Quantidade: </h3>
+                                <h3> Valor Total: </h3>
+                            </div>
+                        </div>
+                        <div >
+                         <img id="cerveja" src={cerveja} alt="cerveja"/>
+                         <div id="escrita-cerveja">
+                                <h3> Quantidade: </h3>
+                                <h3> Valor Total: </h3>
+                            </div>
+                        </div>
+                        <div >
+                         <img id="linguica" src={linguica} alt="linguica"/>
+                         <div id="escrita-linguica">
+                                <h3> Quantidade: </h3>
+                                <h3> Valor Total: </h3>
+                            </div>
+                        </div>
+                        <div >
+                          <img id="frango" src={frango} alt="frango"/>
+                          <div id="escrita-frango">
+                                <h3> Quantidade: </h3>
+                                <h3> Valor Total: </h3>
+                            </div>
+                        </div>
+                        <div >
+                         <img id="refrigerante" src={refrigerante} alt="refrigerante"/>
+                         <div id="escrita-refrigerante">
+                                <h3> Quantidade: </h3>
+                                <h3> Valor Total: </h3>
+                            </div>
+                        </div>
+                       
+                        </div>
+                        </div>
+                        </div>
+                </div>
+
         </Template>
     );
 }
